fa66df537c8e393e23d9327209b2175b44678057
le_shtroumpf <timotheelinocent@gmail.com>
2022-01-12 10:41:14 +0100

    First commit: RecycleView

---
+++ recycle_view.py
@@ -0,0 +1,21 @@
+import kivy
+
+from kivy.uix.recycleview import RecycleView
+from kivy.app import App
+
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in range(10)]
+
+
+class TestApp(App):
+    def build(self):
+        return RV()
+
+
+test = TestApp()
+
+if __name__ == "__main__":
+    test.run()
