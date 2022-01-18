3f1d79b40370c8851c54d6c5bcb864daca2aecdc
le_shtroumpf <timotheelinocent@gmail.com>
2022-01-18 16:08:47 +0100

    #12 Refactor of login app.

--- log.py
+++ log.py
@@ -1,13 +1,17 @@
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen
 from kivy.app import App
 from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 
 class LoginScreen(Screen):
-    def change(self):
-        self.manager.current = "home"
+    def login(self):
+        app.change_to('home')
+
+    def wrong_id(self):
+        self.error = "Wrong username or password, try again:"
 
 
 class LoginBox(BoxLayout):
@@ -20,31 +24,17 @@ class LoginBox(BoxLayout):
     def on_error(self):
         pass
 
-    def get_data(self):
-        fields_user = self.ids.username
-        fields_password = self.ids.password
-        for fields in reversed(fields_user.children):
-            for n in fields.children:
-                if isinstance(n, TextInput):
-                    username = n.text
-                    n.text = ""
-        for fields in reversed(fields_password.children):
-            for m in fields.children:
-                if isinstance(m, TextInput):
-                    password = m.text
-                    m.text = ""
-        self.to_log(username, password)
-
     def to_log(self, username, password):
+        app = App.get_running_app()
+
         if (username, password) == ("test", "1234"):
             self.dispatch('on_login_success')
-            log.username = username
-            self.reset()
-        else:
-            self.dispatch('on_error')
+            app.username = username
+
+        self.dispatch('on_error')
 
 
-class Home(Screen):
+class HomeScreen(Screen):
     """Second screen with log out button"""
     pass
 
@@ -53,14 +43,12 @@ class LogApp(App):
     username = StringProperty("")
 
     def build(self):
-        sm = ScreenManager()
+        self.sm = self.root.ids['screenmanager']
 
-        sm.add_widget(LoginScreen(name="log"))
-        sm.add_widget(Home(name="home"))
-        return sm
+    def change_to(self, screen_name):
+        self.sm.current = screen_name
 
-
-log = LogApp()
 
 if __name__ == "__main__":
-    log.run()
+    app = LogApp()
+    app.run()
